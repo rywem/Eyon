93f12349ddb424cab8901bc52dfc0f8e6b9a24cc
Ryan <cybervoid41@gmail.com>
2019-11-30 07:53:56 -0800

    remove project

--- EyonSolution/Eyon.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Eyon.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- EyonSolution/Eyon.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Eyon.Models
-{
-    public class Class1
-    {
-    }
-}
