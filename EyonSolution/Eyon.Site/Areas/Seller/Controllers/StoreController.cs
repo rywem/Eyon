f8c45074ade2016f095b0d6a87e80d24827252df
Ryan <cybervoid41@gmail.com>
2019-11-30 08:20:13 -0800

    Added StoreController initial stub

---
+++ EyonSolution/Eyon.Site/Areas/Seller/Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eyon.Site.Areas.Seller.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
