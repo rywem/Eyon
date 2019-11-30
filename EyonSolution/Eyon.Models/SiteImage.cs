ac0e2548c05a8fc147038e11754ae370f6258ce9
Ryan <cybervoid41@gmail.com>
2019-11-30 13:34:59 -0800

    Moved GetImage to SiteImage model.

--- EyonSolution/Eyon.Models/SiteImage.cs
+++ EyonSolution/Eyon.Models/SiteImage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Eyon.Models
@@ -12,5 +13,22 @@ namespace Eyon.Models
         public string Title { get; set; }
         public string FileType { get; set; }
         public string Encoded { get; set; }
+
+        
+        public string GetImage()
+        {
+            string imgSrc = string.Empty;
+            if (!string.IsNullOrEmpty(Encoded) && !string.IsNullOrEmpty(FileType))
+            {
+                var base64 = Encoded;
+                string type = string.Empty;
+                if (FileType.Contains("."))
+                    type = FileType.Trim('.');
+
+                imgSrc = string.Format("data:image/{0};base64,{1}", type, base64);
+            }
+
+            return imgSrc;
+        }
     }
 }
--- EyonSolution/Eyon.Models/ViewModels/CategoryViewModel.cs
+++ EyonSolution/Eyon.Models/ViewModels/CategoryViewModel.cs
@@ -9,26 +9,5 @@ namespace Eyon.Models.ViewModels
         public Category Category { get; set; }
         public SiteImage SiteImage { get; set; }
 
-        public string Image 
-        {
-            get
-            {
-                return GetImage();
-            }            
-        }
-
-        public string GetImage()
-        {
-            string imgSrc = string.Empty;
-            if (SiteImage != null)
-            {
-                var base64 = SiteImage.Encoded;
-
-                string type = SiteImage.FileType.Trim('.');
-                imgSrc = string.Format("data:image/{0};base64,{1}", type, base64);
-            }
-
-            return imgSrc;
-        }
     }
 }
