dc63cb2daaf9a5496229ee51316cd7460d678008
Ryan <cybervoid41@gmail.com>
2019-12-01 11:47:28 -0800

    Added CookbookController, Upsert, Insert, Delete action methods, added Upsert and Insert Views, added cookbook.js

--- EyonSolution/Eyon.Site/Areas/Seller/Controllers/CookbookController.cs
+++ EyonSolution/Eyon.Site/Areas/Seller/Controllers/CookbookController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Eyon.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eyon.Site.Areas.Seller.Controllers
@@ -9,14 +11,72 @@ namespace Eyon.Site.Areas.Seller.Controllers
     [Area("Seller")]
     public class CookbookController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public Cookbook cookbook { get; set; }
+        public CookbookController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult Upsert(long? id)
+        {
+            Cookbook cookbook = new Cookbook();            
+            if (id == null)
+                return View(cookbook);
+
+            if (id != null)
+            {
+                cookbook = _unitOfWork.Cookbook.GetFirstOrDefault(x => x.Id == id.GetValueOrDefault());
+            }
+
+            if (cookbook == null )
+                return NotFound();
+            return View(cookbook);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Upsert()
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (cookbook.Id == 0)
+                    _unitOfWork.Cookbook.Add(cookbook);
+                else
+                    _unitOfWork.Cookbook.Update(cookbook);
+
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cookbook);
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Cookbook.Get(id);
+
+            if (objFromDb == null)
+                return Json(new { success = false, message = "Error while deleting, Id does not exist. " });
+
+            _unitOfWork.Cookbook.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful." });
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Cookbook.GetAll() });
         }
     }
 }
