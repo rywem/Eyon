ddd8eb078e0baad9d7e2b9a586726a8c960334ba
Ryan <cybervoid41@gmail.com>
2019-12-09 07:10:49 -0800

    Removed UnitTests project, replaced with XUnit "XTests", added integration test project

---
+++ EyonSolution/Eyon.XTests.IntegrationsTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Eyon.XTests.IntegrationsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ EyonSolution/Eyon.XTests.UnitTests/DataAccess/Data/CategoryRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Eyon.DataAccess.Data.Repository;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Eyon.DataAccess.Data;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data
+{
+
+    public class CategoryRepositoryTests
+    {
+        ICategoryRepository categoryRepository = GetInMemoryRepository();
+        [Fact]
+        public void AddRepository_AssertCategoryAdded()
+        {
+
+        }
+
+        //https://www.carlrippon.com/testing-ef-core-repositories-with-xunit-and-an-in-memory-db/
+        private ICategoryRepository GetInMemoryRepository()
+        {            
+            DbContextOptions<Eyon.DataAccess.Data.ApplicationDbContext> options;
+            var builder = new DbContextOptionsBuilder<Eyon.DataAccess.Data.ApplicationDbContext>();
+            builder.UseInMemoryDatabase("Eyon_Test");
+            options = builder.Options;
+            ApplicationDbContext dbContext = new ApplicationDbContext(options);            
+            dbContext.Database.EnsureCreated();
+            dbContext.Database.EnsureDeleted();
+            return new CategoryRepository(dbContext);
+        }
+    }
+}
