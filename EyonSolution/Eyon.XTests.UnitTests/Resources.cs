72697e56e6eaf60c076c2d257ffc2d7e489ec5cf
Ryan <cybervoid41@gmail.com>
2019-12-14 10:43:56 -0800

    Added UnitTests.Resources file, moved code to create In memory db to resources

---
+++ EyonSolution/Eyon.XTests.UnitTests/Resources.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Eyon.DataAccess.Data;
+using Eyon.DataAccess.Data.Repository;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eyon.XTests.UnitTests
+{
+    public class Resources
+    {
+        //https://www.carlrippon.com/testing-ef-core-repositories-with-xunit-and-an-in-memory-db/
+
+        /// <summary>
+        /// Creates a new Instance of an in memory database
+        /// </summary>
+        /// <param name="inMemoryDatabaseName">The In Memory database name</param>
+        /// <returns>The UnitOfWork</returns>
+        public IUnitOfWork GetInMemoryUnitOfWork(string inMemoryDatabaseName)
+        {
+            DbContextOptions<Eyon.DataAccess.Data.ApplicationDbContext> options;
+            var builder = new DbContextOptionsBuilder<Eyon.DataAccess.Data.ApplicationDbContext>();
+            builder.UseInMemoryDatabase(inMemoryDatabaseName);
+            options = builder.Options;
+            ApplicationDbContext dbContext = new ApplicationDbContext(options);
+            dbContext.Database.EnsureCreated();
+            dbContext.Database.EnsureDeleted();
+            return new UnitOfWork(dbContext);
+
+        }
+    }
+}
