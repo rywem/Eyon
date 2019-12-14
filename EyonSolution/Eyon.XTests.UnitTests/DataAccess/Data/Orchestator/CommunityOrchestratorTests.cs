2bbfe6e4d872f4fd493a51e28596bd3e9ba18305
Ryan <cybervoid41@gmail.com>
2019-12-14 13:43:27 -0800

    Added Stub CommunityRespositoryTests, CommunityOrchestratorTests,

--- EyonSolution/Eyon.XTests.UnitTests/DataAccess/Data/Orchestator/CommunityOrchestratorTests.cs
+++ EyonSolution/Eyon.XTests.UnitTests/DataAccess/Data/Orchestator/CommunityOrchestratorTests.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Eyon.DataAccess.Data.Orchestrators;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Eyon.XTests.UnitTests.DataAccess.Data.Orchestator
 {
-    class CommunityOrchestratorTests
+    public class CommunityOrchestratorTests
     {
+        IUnitOfWork _unitOfWork;
+        CommunityOrchestrator _orchestrator;
+        public CommunityOrchestratorTests()
+        {
+            this._unitOfWork = new Resources().GetInMemoryUnitOfWork(nameof(CommunityOrchestratorTests));
+            this._orchestrator = new Eyon.DataAccess.Data.Orchestrators.CommunityOrchestrator(_unitOfWork);
+            SeedDatabase();
+        }
+
+        private void SeedDatabase()
+        {
+
+        }
     }
 }
---
+++ EyonSolution/Eyon.XTests.UnitTests/DataAccess/Data/Repository/CommunityRepositoryTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data.Repository
+{
+    public class CommunityRepositoryTests
+    {
+    }
+}
