93e3120e803993da6a8ddd6dbe70025d1ec568c8
Ryan <cybervoid41@gmail.com>
2019-12-17 07:12:39 -0800

    Added migration to database

---
+++ Eyon.DataAccess/Migrations/20191217150922_AddOrganizationToDatabase.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eyon.DataAccess.Migrations
+{
+    public partial class AddOrganizationToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organization",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Type = table.Column<string>(nullable: false),
+                    Website = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organization", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommunityOrganizations",
+                columns: table => new
+                {
+                    CommunityId = table.Column<long>(nullable: false),
+                    OrganizationId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommunityOrganizations", x => new { x.OrganizationId, x.CommunityId });
+                    table.ForeignKey(
+                        name: "FK_CommunityOrganizations_Community_CommunityId",
+                        column: x => x.CommunityId,
+                        principalTable: "Community",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CommunityOrganizations_Organization_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organization",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrganizationApplicationUser",
+                columns: table => new
+                {
+                    OrganizationId = table.Column<long>(nullable: false),
+                    ApplicationUserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationApplicationUser", x => new { x.ApplicationUserId, x.OrganizationId });
+                    table.ForeignKey(
+                        name: "FK_OrganizationApplicationUser_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrganizationApplicationUser_Organization_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organization",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrganizationCookbooks",
+                columns: table => new
+                {
+                    CookbookId = table.Column<long>(nullable: false),
+                    OrganizationId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationCookbooks", x => new { x.OrganizationId, x.CookbookId });
+                    table.ForeignKey(
+                        name: "FK_OrganizationCookbooks_Cookbook_CookbookId",
+                        column: x => x.CookbookId,
+                        principalTable: "Cookbook",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrganizationCookbooks_Organization_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organization",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Quincy,_California");
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Myrtle_Beach,_South_Carolina");
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Deer_River,_Minnesota");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityOrganizations_CommunityId",
+                table: "CommunityOrganizations",
+                column: "CommunityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationApplicationUser_OrganizationId",
+                table: "OrganizationApplicationUser",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationCookbooks_CookbookId",
+                table: "OrganizationCookbooks",
+                column: "CookbookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommunityOrganizations");
+
+            migrationBuilder.DropTable(
+                name: "OrganizationApplicationUser");
+
+            migrationBuilder.DropTable(
+                name: "OrganizationCookbooks");
+
+            migrationBuilder.DropTable(
+                name: "Organization");
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Quincy,_California");
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Myrtle_Beach,_South_Carolina");
+
+            migrationBuilder.UpdateData(
+                table: "Community",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "WikipediaURL",
+                value: "https://en.wikipedia.org/wiki/Deer_River,_Minnesota");
+        }
+    }
+}
