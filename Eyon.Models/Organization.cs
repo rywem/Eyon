7511ca3a9fbef0752a2ac407fad2842a01aaf13b
Ryan <cybervoid41@gmail.com>
2019-12-17 06:18:26 -0800

    Added properties to Organization Model

--- Eyon.Models/Organization.cs
+++ Eyon.Models/Organization.cs
@@ -8,15 +8,18 @@ namespace Eyon.Models
 {
     public class Organization
     {
-        /*
         [Key]
         public long Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }
+        [Required]
+        public string Type { get; set; }
+        public string Website { get; set; }
         public ICollection<CommunityOrganizations> CommunityOrganizations { get; set; }
-        public ICollection<OrganizationRecipes> OrganizationRecipes { get; set; }
-        */
+        public ICollection<CommunityOrganizations> OrganizationCookbooks { get; set; }
+        //public ICollection<OrganizationRecipes> OrganizationRecipes { get; set; }
+
     }
 }
