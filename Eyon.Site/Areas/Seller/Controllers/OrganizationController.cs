71aa6f01dc3ed84f576408c9e8937eefcea8a83d
Ryan <cybervoid41@gmail.com>
2019-12-19 07:00:36 -0800

    added organization upsert

--- Eyon.Site/Areas/Seller/Controllers/OrganizationController.cs
+++ Eyon.Site/Areas/Seller/Controllers/OrganizationController.cs
@@ -18,8 +18,10 @@ namespace Eyon.Site.Areas.Seller.Controllers
         private OrganizationOrchestrator organizationOrchestrator;
         [BindProperty]
         public OrganizationViewModel organizationViewModel { get; set; }
-        public OrganizationController()
+        public OrganizationController( IUnitOfWork unitOfWork )
         {
+            this._unitOfWork = unitOfWork;
+            this.organizationOrchestrator = new OrganizationOrchestrator(_unitOfWork);
 
         }
         public IActionResult Index()
@@ -34,6 +36,40 @@ namespace Eyon.Site.Areas.Seller.Controllers
             else
                 organizationViewModel = organizationOrchestrator.GetOrganizationViewModel(id.GetValueOrDefault());
 
+            return View(organizationViewModel);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Upsert()
+        {
+            if ( ModelState.IsValid )
+            {
+                //todo validate the user submitting has permission to add or edit this cookbook.
+                try
+                {
+                    if ( organizationViewModel.Organization.Id == 0 ) //New cookbook
+                    {
+                        organizationOrchestrator.AddOrganizationTransaction(organizationViewModel);
+                    }
+                    else
+                    {
+                        organizationOrchestrator.UpdateOrganizationTransaction(organizationViewModel);
+                    }
+                }
+                catch ( Models.Errors.WebUserSafeException usEx )
+                {
+                    ModelState.AddModelError(string.Empty, usEx.Message);
+                    return View(organizationViewModel);
+                }
+                catch ( Exception ex )
+                {
+                    ModelState.AddModelError(String.Empty, "An error occurred.");
+                    return View(organizationViewModel);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
             return View(organizationViewModel);
         }
     }
