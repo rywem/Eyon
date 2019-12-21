6808f4a63a1d37fc2fd1666a54320cb0b8327f5e
Ryan <cybervoid41@gmail.com>
2019-12-21 09:06:21 -0800

    Added recipe view model

---
+++ Eyon.Models/ViewModels/RecipeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace Eyon.Models.ViewModels
+{
+    public class RecipeViewModel
+    {
+        public Recipe Recipe { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+        public List<Instruction> Instructions { get; set; }
+        public Community Community { get; set; }
+        public List<SiteImage> RecipeSiteImages { get; set; }
+        public List<Cookbook> Cookbooks { get; set; }
+        //public List<Organization> Organizations { get; set; }
+        public RecipeViewModel()
+        {
+            this.Recipe = new Recipe();
+            this.Community = new Community();
+            this.Categories = new List<Category>();
+            this.Ingredients = new List<Ingredient>();
+            this.Instructions = new List<Instruction>();
+            this.RecipeSiteImages = new List<SiteImage>();
+            this.Cookbooks = new List<Cookbook>();
+            //this.Organizations = new List<Organization>();
+        }
+
+    }
+}
---
+++ Eyon.XTests.UnitTests/DataAccess/Data/Orchestator/RecipeOrchestratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Eyon.DataAccess.Data.Repository;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Eyon.DataAccess.Data;
+using System;
+using Eyon.Models.ViewModels;
+using Eyon.DataAccess.Data.Orchestrators;
+using System.Linq;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data.Orchestator
+{
+    public class RecipeOrchestratorTests : IDisposable
+    {
+
+        IUnitOfWork _unitOfWork;
+        RecipeOrchestrator _orchestrator;
+        public RecipeOrchestratorTests()
+        {
+            this._unitOfWork = new Resources().GetInMemoryUnitOfWork(nameof(RecipeOrchestratorTests));
+            this._orchestrator = new Eyon.DataAccess.Data.Orchestrators.RecipeOrchestrator(_unitOfWork);
+            SeedDatabase();
+        }
+
+        private void SeedDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+    }
+}
