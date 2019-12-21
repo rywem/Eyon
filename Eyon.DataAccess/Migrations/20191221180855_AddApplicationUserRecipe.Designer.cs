cfbae7d81f7d65af57116c10c05fcd4269cd4d33
Ryan <cybervoid41@gmail.com>
2019-12-21 10:10:22 -0800

    Added ApplicationUserRecipe migration

--- Eyon.DataAccess/Migrations/20191221180855_AddApplicationUserRecipe.Designer.cs
+++ Eyon.DataAccess/Migrations/20191221180855_AddApplicationUserRecipe.Designer.cs
@@ -4,14 +4,16 @@ using Eyon.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Eyon.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191221180855_AddApplicationUserRecipe")]
+    partial class AddApplicationUserRecipe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
