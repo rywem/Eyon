9436e438b2bccc14515cd65fc431efb66f2480bb
Ryan <cybervoid41@gmail.com>
2019-12-21 07:44:51 -0800

    Added InstructionRepository, IInstructionRepository object

---
+++ Eyon.DataAccess/Data/Repository/IRepository/IInstructionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Eyon.Models;
+
+namespace Eyon.DataAccess.Data.Repository.IRepository
+{
+    public interface IInstructionRepository : IRepository<Instruction>
+    {
+
+        void Update( Instruction instruction );
+    }    
+}
---
+++ Eyon.DataAccess/Data/Repository/InstructionRepository.cs
@@ -0,0 +1,29 @@
+﻿using Eyon.DataAccess.Data.Repository.IRepository;
+using Eyon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eyon.DataAccess.Data.Repository
+{
+    public class InstructionRepository : Repository<Instruction>, IInstructionRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InstructionRepository( ApplicationDbContext db ) : base(db)
+        {
+            this._db = db;
+        }
+
+        public void Update( Instruction instruction )
+        {
+            var objFromDb = _db.Instruction.FirstOrDefault(s => s.Id == instruction.Id);
+            objFromDb.StepNumber = instruction.StepNumber;
+            objFromDb.Text = instruction.Text;
+            
+            _db.SaveChanges();
+        }
+
+    }
+}
