29d1832877c67fde505b1a23e641a18d78a358a0
Ryan <cybervoid41@gmail.com>
2019-12-26 06:41:09 -0800

    Community partially implemented search

--- Eyon.DataAccess/Data/Repository/CommunityRepository.cs
+++ Eyon.DataAccess/Data/Repository/CommunityRepository.cs
@@ -26,6 +26,26 @@ namespace Eyon.DataAccess.Data.Repository
             });
         }
 
+        public IEnumerable<Community> Search( string searchString, string includeProperties = null )
+        {
+            //IQueryable<Community> query
+            /*var query = (from c in _db.Community
+                        join cs in _db.CommunityState
+                        on c.Id equals cs.CommunityId into csc
+                        from csci in csc.DefaultIfEmpty()
+                        join s in _db.State
+                        on csci.StateId equals s.Id into stateCS
+                        from scsc in stateCS.DefaultIfEmpty()
+                        where ( c.Name.Contains(searchString)
+                            || scsc.Name.Contains(searchString)
+                            || scsc.Code.Contains(searchString)
+                            || scsc.LocalName.Contains(searchString) )
+                        select c);
+            */
+            return null;
+
+        }
+
         public void Update(Community community)
         {
             var objFromDb = _db.Community.FirstOrDefault(s => s.Id == community.Id);
--- Eyon.DataAccess/Data/Repository/IRepository/ICommunityRepository.cs
+++ Eyon.DataAccess/Data/Repository/IRepository/ICommunityRepository.cs
@@ -6,7 +6,7 @@ namespace Eyon.DataAccess.Data.Repository.IRepository
     public interface ICommunityRepository : IRepository<Models.Community>
     {
         IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> GetCategoryListForDropDown();
-
+        IEnumerable<Community> Search( string searchString, string includeProperties = null );
         void Update(Community community);
     }    
 }
