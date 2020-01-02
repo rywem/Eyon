3aafb849a1f9ce97c1ffafe37b5f11eb564f3393
Ryan <cybervoid41@gmail.com>
2020-01-02 05:39:54 -0800

    added styling to search box

---
+++ Eyon.Site/wwwroot/js/community/recipe_community_search.js
@@ -0,0 +1,40 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    //loadDataTable();
+});
+
+
+function loadDataTable(searchString) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/user/community/searchCommunities?searchString=" + searchString,
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "communityName", "width": "15%" },
+            { "data": "stateName", "width": "15%" },
+            { "data": "countryName", "width": "15%" },            
+            {
+                "data": "communityId",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <input type="button" class='badge badge-secondary text-white' style='cursor:pointer; width:100px' onclick="chooseCommunity(${data})">
+                                    <i class="fas fa-hand-pointer"></i> Select Community
+                                </input>                                
+                            </div>
+                            `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No records found"
+        },
+        "width": "100%"
+    });
+}
+
+function chooseCommunity(communityId) {
+    alert('not implemented');
+}
