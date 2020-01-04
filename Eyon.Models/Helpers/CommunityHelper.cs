c6cd4d7084f828662a308d82bed963854430d4df
Ryan <cybervoid41@gmail.com>
2020-01-04 06:42:50 -0800

    Added communityhelper with method to format a community string into full name

---
+++ Eyon.Models/Helpers/CommunityHelper.cs
@@ -0,0 +1,38 @@
+﻿using Eyon.Utilities.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eyon.Models.Helpers
+{
+    public static class CommunityHelper
+    {
+        /// <summary>
+        /// Formats the communtiy name.
+        /// </summary>        
+        /// <returns>ex Quincy, CA (United States)</returns>
+        public static string FullNameFormatter(Community community)
+        {
+            return CommunityFullNameFormatter(community.Name, community.CommunityState != null && community.CommunityState.State != null ? community.CommunityState.State.Name : string.Empty,
+                community.CommunityState != null && community.CommunityState.State != null ? community.CommunityState.State.Code : string.Empty, community.Country.Name);
+        }
+
+        private static string CommunityFullNameFormatter( string community, string state, string code, string country )
+        {
+            string stateFormatted = string.Empty;
+            if ( !string.IsNullOrEmpty(code) )
+                stateFormatted = code;
+            else if ( !string.IsNullOrEmpty(state) )
+                stateFormatted = state.ToProperCase();
+
+            if ( !string.IsNullOrEmpty(stateFormatted) )
+                stateFormatted = string.Format(", {0}", stateFormatted);
+
+            string countryFormatted = string.Empty;
+            if ( !string.IsNullOrEmpty(country) )
+                countryFormatted = string.Format(" ({0})", country.ToProperCase());
+
+            return string.Format("{0}{1}{2}", community.ToProperCase(), stateFormatted, countryFormatted);
+        }
+    }
+}
