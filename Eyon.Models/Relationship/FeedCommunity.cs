167b887786f1573534253f60747adc19bae73f44
Ryan <cybervoid41@gmail.com>
2020-03-07 08:20:04 -0800

    Added FeedCategory and FeedCommunity many to many relationship tables

---
+++ Eyon.Models/Relationship/FeedCommunity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Eyon.Models.Relationship
+{
+    public class FeedCommunity 
+    {
+        public long FeedId { get; set; }
+        [ForeignKey("FeedId")]
+        public Feed Feed { get; set; }
+        public long CommunityId { get; set; }
+        [ForeignKey("CommunityId")]
+        public Community Community { get; set; }
+    }
+}
