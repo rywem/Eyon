a0ec55b53c80959d8f48eedfbd6bae7bb0f2d916
Ryan <cybervoid41@gmail.com>
2020-03-07 12:24:29 -0800

    FeedProfile migration added

---
+++ Eyon.DataAccess/Migrations/20200307200623_FeedProfileAdded.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eyon.DataAccess.Migrations
+{
+    public partial class FeedProfileAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeedUser");
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CreationDateTime = table.Column<DateTime>(nullable: false),
+                    ModifiedDateTime = table.Column<DateTime>(nullable: false),
+                    ApplicationUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profile_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FeedProfile",
+                columns: table => new
+                {
+                    FeedId = table.Column<long>(nullable: false),
+                    ProfileId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedProfile", x => new { x.FeedId, x.ProfileId });
+                    table.ForeignKey(
+                        name: "FK_FeedProfile_Feed_FeedId",
+                        column: x => x.FeedId,
+                        principalTable: "Feed",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FeedProfile_Profile_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedProfile_ProfileId",
+                table: "FeedProfile",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_ApplicationUserId",
+                table: "Profile",
+                column: "ApplicationUserId",
+                unique: true,
+                filter: "[ApplicationUserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeedProfile");
+
+            migrationBuilder.DropTable(
+                name: "Profile");
+
+            migrationBuilder.CreateTable(
+                name: "FeedUser",
+                columns: table => new
+                {
+                    FeedId = table.Column<long>(type: "bigint", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedUser", x => new { x.FeedId, x.ApplicationUserId });
+                    table.ForeignKey(
+                        name: "FK_FeedUser_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FeedUser_Feed_FeedId",
+                        column: x => x.FeedId,
+                        principalTable: "Feed",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedUser_ApplicationUserId",
+                table: "FeedUser",
+                column: "ApplicationUserId");
+        }
+    }
+}
