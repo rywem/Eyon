a0ef8954b0beb72dbe893a220d62de2fe40386eb
Ryan <cybervoid41@gmail.com>
2020-03-08 06:38:13 -0700

    Added FeedCategoryRespository, FeedCommunityRepository, FeedCookbookRepository

---
+++ Eyon.DataAccess/Data/Repository/Relationship/FeedCommunityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Eyon.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Linq;
+using Eyon.Models.Relationship;
+using Eyon.Models.Errors;
+
+namespace Eyon.DataAccess.Data.Repository
+{
+    public class FeedCommunityRepository : Repository<FeedCommunity>, IFeedCommunityRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FeedCommunityRepository( ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+
+        public override void Add( FeedCommunity entityToAdd )
+        {
+            if (_db.FeedCommunity.Any(x => x.FeedId == entityToAdd.FeedId && x.CommunityId == entityToAdd.CommunityId) )
+                throw new SafeException("An error ocurred.", new Exception(string.Format("FeedCategory already exists. FeedId {0},  CategoryId {1}", entityToAdd.FeedId, entityToAdd.CommunityId)));
+
+            base.Add(entityToAdd);
+        }
+    }
+}
---
+++ Eyon.DataAccess/Data/Repository/Relationship/FeedCookbookRepository.cs
@@ -0,0 +1,26 @@
+﻿using Eyon.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Linq;
+using Eyon.Models.Relationship;
+using Eyon.Models.Errors;
+
+namespace Eyon.DataAccess.Data.Repository
+{
+    public class FeedCookbookRepository : Repository<FeedCookbook>, IFeedCookbookRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FeedCookbookRepository( ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+
+        public override void Add( FeedCookbook entityToAdd )
+        {
+            if (_db.FeedCookbook.Any(x => x.FeedId == entityToAdd.FeedId && x.CookbookId == entityToAdd.CookbookId) )
+                throw new SafeException("An error ocurred.", new Exception(string.Format("FeedCategory already exists. FeedId {0},  CategoryId {1}", entityToAdd.FeedId, entityToAdd.CookbookId)));
+
+            base.Add(entityToAdd);
+        }
+    }
+}
