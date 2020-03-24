3da7ecc902d2539a17d14eb4b710982529f40ebd
Ryan <cybervoid41@gmail.com>
2020-03-24 05:42:30 -0700

    Added test ApplicationUserCookbookRepositoryTests

---
+++ Eyon.XTests.UnitTests/DataAccess/Data/Repository/Relationship/ApplicationUserCookbookRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using System;
+using Eyon.DataAccess.Data.Repository;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using System.Collections.Generic;
+using System.Text;
+using Eyon.Models;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data.Repository.Relationship
+{
+    public class ApplicationUserCookbookRepositoryTests : IDisposable
+    {
+        IUnitOfWork _unitOfWork;
+        public ApplicationUserCookbookRepositoryTests()
+        {
+            this._unitOfWork = new Resources().GetInMemoryUnitOfWork(nameof(ApplicationUserCookbookRepositoryTests));
+        }
+
+        [Fact]
+        public void AddApplicationUserCookbook_FromEntities()
+        {
+            Guid guid = new Guid();
+
+            ApplicationUser applicationUser = new ApplicationUser()
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+            Cookbook cookbook = new Cookbook()
+            {
+                Id = 1
+            };
+
+            var entity = _unitOfWork.ApplicationUserCookbook.AddFromEntities(applicationUser, cookbook);
+            Assert.Equal(applicationUser.Id, entity.ApplicationUserId);
+            Assert.Equal(cookbook.Id, entity.ObjectId);
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+    }
+}
