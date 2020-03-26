1f5f98a75a937ded7d162f9ba7e59d8cfbbfd0ba
Ryan <cybervoid41@gmail.com>
2020-03-26 05:44:11 -0700

    ManyToMany repository tests

---
+++ Eyon.XTests.UnitTests/DataAccess/Data/Repository/Relationship/OrganizationCookbookRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Eyon.Models;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data.Repository.Relationship
+{
+    public class OrganizationCookbookRepositoryTests : IDisposable
+    {
+        IUnitOfWork _unitOfWork;
+        public OrganizationCookbookRepositoryTests()
+        {
+            this._unitOfWork = new Resources().GetInMemoryUnitOfWork(nameof(OrganizationCookbookRepositoryTests));
+        }
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+        [Fact]
+        public void AddOrganizationCookbook_FromEntities()
+        {
+            var firstEntity = new Organization()
+            {
+                Id = 1
+            };
+            var secondEntity = new Cookbook()
+            {
+                Id = 3
+            };
+
+            var entityRelation = _unitOfWork.OrganizationCookbook.AddFromEntities(firstEntity, secondEntity);
+            Assert.Equal(firstEntity.Id, entityRelation.OrganizationId);
+            Assert.Equal(secondEntity.Id, entityRelation.CookbookId);
+        }
+    }
+}
---
+++ Eyon.XTests.UnitTests/DataAccess/Data/Repository/Relationship/RecipeCategoryRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using Eyon.DataAccess.Data.Repository.IRepository;
+using Eyon.Models;
+
+namespace Eyon.XTests.UnitTests.DataAccess.Data.Repository.Relationship
+{
+    public class RecipeCategoryRepositoryTests : IDisposable
+    {
+        IUnitOfWork _unitOfWork;
+        public RecipeCategoryRepositoryTests()
+        {
+            this._unitOfWork = new Resources().GetInMemoryUnitOfWork(nameof(RecipeCategoryRepositoryTests));
+        }
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+        [Fact]
+        public void AddRecipeCategory_FromEntities()
+        {
+            var firstEntity = new Recipe()
+            {
+                Id = 1
+            };
+            var secondEntity = new Category()
+            {
+                Id = 3
+            };
+
+            var entityRelation = _unitOfWork.RecipeCategory.AddFromEntities(firstEntity, secondEntity);
+            Assert.Equal(firstEntity.Id, entityRelation.RecipeId);
+            Assert.Equal(secondEntity.Id, entityRelation.CategoryId);
+        }
+    }
+}
